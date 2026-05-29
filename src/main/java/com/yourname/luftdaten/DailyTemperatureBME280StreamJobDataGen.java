960c2059a05f19ed2f58b2b95fa9e80fe12d40d0
murat <murar390@gmail.com>
2026-05-29 10:34:47 +0200

    new job with TCP source and sink

---
+++ src/main/java/com/yourname/luftdaten/DailyTemperatureBME280StreamJobDataGen.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.yourname.luftdaten;
+
+import java.time.Duration;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.file.src.FileSource;
+import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import com.yourname.luftdaten.entities.BME280Reading;
+
+/**
+ * Skeleton for a Flink DataStream Job.
+ *
+ * <p>For a tutorial how to write a Flink application, check the
+ * tutorials and examples on the <a href="https://flink.apache.org">Flink Website</a>.
+ *
+ * <p>To package your application into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ *
+ * <p>If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+public class DailyTemperatureBME280StreamJobDataGen {
+    public static void main(String[] args) throws Exception {
+        // Sets up the execution environment, which is the main entry point
+        // to building Flink applications.
+
+        if (args.length < 4) {
+            System.err.println("Usage: DataStreamJob <host producing input> <port producing input> <sink host> <port sink>");
+            return;
+        }
+
+        String sourceHost = args[0], sinkHost = args[2];
+        int sourcePort = Integer.parseInt(args[1]), sinkPort = Integer.parseInt(args[3]);
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStream<String> stream = env.socketTextStream(sourceHost, sourcePort);
+        DataStream<String> filteredLines = stream.filter(line -> !line.startsWith("sensor_id"));
+        DataStream<BME280Reading> correctReadings = filteredLines
+                .map(BME280Parser::parseReading)
+                .filter(reading -> reading.getSensor_id() != null && reading.getTemperature() != null);
+        DataStream<BME280Reading> withTimestamps = correctReadings.assignTimestampsAndWatermarks(WatermarkStrategy.<BME280Reading>forMonotonousTimestamps().withTimestampAssigner((reading, timestamp) -> reading.getTimestamp().toEpochMilli()));
+        DataStream<String> tempAverageHourly = withTimestamps
+                .keyBy(BME280Reading::getSensor_id)
+                .window(TumblingEventTimeWindows.of(Duration.ofMinutes(1)))
+                .aggregate(new AggregateAverage(), new AverageResultWindowFunction());
+        tempAverageHourly.map(s -> s + "\n")
+                .writeToSocket(sinkHost, sinkPort, new SimpleStringSchema());
+
+    // Execute program, beginning computation.
+        env.execute("Daily Temperature from BME280 sensors with data from socket and writing results to socket");
+    }
+}
